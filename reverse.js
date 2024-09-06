71a9905ec18c4727e0d907231a849975f80d1840
mahmudul-hasan <mhmahmudul1211@gmail.com>
2024-09-06 15:47:47 +0600

    first commit

---
+++ reverse.js
@@ -0,0 +1,8 @@
+const sentence = 'Hi im Mahmudul Hasan Molla'
+
+let reverse = '' 
+for (const letter of sentence){
+    // console.log(letter) // for no change 
+    reverse = letter + reverse;
+}
+    console.log(reverse); 
